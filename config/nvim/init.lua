fe5df843325ce99ca6a9a2f94794b821bb9e73f9
ludvig <ludvig.westerdahl.tech.v1@protonmail.com>
2026-04-12 19:53:50 +0200

    Added snippet engine to blink plugin

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -673,11 +673,25 @@ require('lazy').setup({
     },
   },
 
-  { -- Autocompletion
+
+  {
+      -- Autocompletion
+      -- See https://cmp.saghen.dev/configuration/general.html
     'saghen/blink.cmp',
     event = 'VimEnter',
     version = '1.*',
-    dependencies = {},
+    dependencies = {
+          -- Snippet Engine
+          {
+            'L3MON4D3/LuaSnip',
+            version = '2.*',
+            build = (function()
+              return 'make install_jsregexp'
+            end)(),
+            dependencies = {},
+            opts = {},
+          },
+        },
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
@@ -726,17 +740,7 @@ require('lazy').setup({
       },
 
       snippets = { preset = 'luasnip' },
-
-      -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
-      -- which automatically downloads a prebuilt binary when enabled.
-      --
-      -- By default, we use the Lua implementation instead, but you may enable
-      -- the rust implementation via `'prefer_rust_with_warning'`
-      --
-      -- See :h blink-cmp-config-fuzzy for more information
-      fuzzy = { implementation = 'lua' },
-
-      -- Shows a signature help window while you type arguments for a function
+      fuzzy = { implementation = 'rust' },
       signature = { enabled = true },
     },
   },
