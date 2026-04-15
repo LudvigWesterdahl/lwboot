ef3c5ea499fdc8731b0be47885e0bbf2aade9b05
ludvig <ludvig.westerdahl.tech.v1@protonmail.com>
2026-04-15 10:09:04 +0200

    Added all ghostty theme colors to groups and styled cursor

--- config/nvim/lua/lwcs/groups.lua
+++ config/nvim/lua/lwcs/groups.lua
@@ -7,24 +7,47 @@ M.setup = function()
 
   local function fg(key, val) mappings[key] = { fg = val } end
 
-  local colorBg = '#282c34'
-  local color4Blue = '#82a2be'
+  local function any(key, val) mappings[key] = val end
+
+  local c = {
+      background = "#282c34",
+      foreground = "#ffffff",
+      cursor = "#ffffff",
+      cursorText = "#353a44",
+      black = "#1d1f21",
+      red = "#cc6566",
+      green = "#b6bd68",
+      yellow = "#f0c674",
+      blue = "#82a2be",
+      magenta = "#b294bb",
+      cyan = "#8abeb7",
+      white = "#c4c8c6",
+      blackBright = "#666666",
+      redBright = "#d54e53",
+      greenBright = "#b9ca4b",
+      yellowBright = "#e7c547",
+      blueBright = "#7aa6da",
+      magentaBright = "#c397d8",
+      cyanBright = "#70c0b1",
+      whiteBright = "#eaeaea"
+  }
 
   -- General
+  any('Normal', {bg = c.background, fg = c.foreground})
+  any("Cursor", {bg = c.cursor, fg = c.cursorText})
+  bg('NormalFloat', c.background)
+  bg('NormalNC', c.background)
 
-  bg('Normal', colorBg)
-  bg('NormalFloat', colorBg)
-  bg('NormalNC', colorBg)
+
+  -- Language: any
+  fg('@lw_keyword', c.blueBright)
 
   -- Language: java
+  fg('@lwjava_import_ref', '#0000FF')
 
   -- Language: c
+  -- TODO
 
-  fg('@string.c', '#00FF00')
-  fg('@lw_string.java', '#F44336')
-  -- fg("@import
-  fg('@lw_keyword', color4Blue)
-  fg('@lw_import_ref', '#0000FF')
   return mappings
 end
 
